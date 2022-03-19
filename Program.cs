54d745f3544b5cf64a971dec9592a79e6974936a
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-19 16:51:46 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace modul4_1302204126
     {
         static void Main(string[] args)
         {
-            
-            
+            SimpleDataBase<int> sdb = new SimpleDataBase<int>();
+
+            sdb.AddNewData(13); sdb.AddNewData(02); sdb.AddNewData(20);
+            sdb.PrintAllData();
         }
     }
 }
