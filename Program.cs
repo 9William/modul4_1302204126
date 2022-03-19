fba68b80b9768bcc0bbebf49eccec23b3668c2f0
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-19 15:41:13 +0700

    awalan

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace modul4_1302204126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
